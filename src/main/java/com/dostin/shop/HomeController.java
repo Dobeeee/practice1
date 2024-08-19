a7f339b35ca7c1484d00fbb3321adf9209612d4e
dostin <wsh0684@naver.com>
2024-08-19 09:53:40 +0900

    구구단 구현

--- src/main/java/com/dostin/shop/HomeController.java
+++ src/main/java/com/dostin/shop/HomeController.java
@@ -1,15 +1,36 @@
 package com.dostin.shop;
 
+import jdk.jfr.Percentage;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class HomeController {
     @RequestMapping("/shop")
     @ResponseBody
-
     public String showHome() {
         return "라이브";
+    }
+
+    @GetMapping("/gugudan")
+    @ResponseBody
+    public String showGugudan(Integer dan, Integer limit) {
+        if (dan == null) {
+            dan = 9;
+        }
+        if (limit == null) {
+            limit = 9;
+        }
+        final Integer finalDan = dan;
+        return IntStream.rangeClosed(1, limit)
+                .mapToObj(i -> "%d * %d = %d".formatted(finalDan, i, finalDan * i))
+                .collect(Collectors.joining("<br>"));
+
+
     }
 }
